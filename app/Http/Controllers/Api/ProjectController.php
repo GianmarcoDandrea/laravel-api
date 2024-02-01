68cd33d693ee3b24d2430f340bfbd183aa291fd4
GianmarcoDandrea <gianm96@hotmail.it>
2024-02-01 16:22:05 +0100

    changed: in Api/ProjectController result in results

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index() {
         $projects = Project::with(['type','technologies'])->paginate(10);
         return response()->json([
-            'result' => $projects,
+            'results' => $projects,
             'success' => true
         ]);
     }
